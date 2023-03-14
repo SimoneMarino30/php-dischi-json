1258c10b9bd25b291bccab7c1d988de684da673b
Simone Marino <simo.marino@outlook.it>
2023-03-14 16:03:50 +0100

    Axios call OK + Card printed via JSON OK

--- frontend/main.js
+++ frontend/main.js
@@ -15,56 +15,16 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      discs_list: [
-        {
-          title: "New Jersey",
-          author: "Bon Jovi",
-          year: 1988,
-          poster:
-            "https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg",
-          genre: "Rock",
-        },
-        {
-          title: "Live at Wembley 86",
-          author: "Queen",
-          year: 1992,
-          poster:
-            "https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg",
-          genre: "Pop",
-        },
-        {
-          title: 'Ten"s Summoner"s Tales',
-          author: "Sting",
-          year: 1993,
-          poster:
-            "https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg",
-          genre: "Pop",
-        },
-        {
-          title: "Steve Gadd band",
-          author: "Steve Gadd Band",
-          year: 2018,
-          poster: "https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg",
-          genre: "Jazz",
-        },
-        {
-          title: "Brave new World",
-          author: "Iron Maiden",
-          year: 2000,
-          poster:
-            "https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg",
-          genre: "Metal",
-        },
-        {
-          title: "One more car, one more rider",
-          author: "Eric Clapton",
-          year: 2002,
-          poster:
-            "https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg",
-          genre: "Rock",
-        },
-      ],
+      discs_list: [],
     };
   },
-  methods: {},
+
+  //   recupero (GET) info dalla API
+  created() {
+    // parte la richiesta per l'array
+    axios
+      .get("http://localhost/php-dischi-json/API/get-list.php")
+      //   'riempio' il mio array vuoto con la resp
+      .then((resp) => (this.discs_list = resp.data));
+  },
 }).mount("#root");
